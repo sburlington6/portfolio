4e1c575703a4baec360c6d5df950c565c3097d16
Richard Bird <sburlington6@yahoo.com>
2016-06-12 23:44:15 -0400

    Isotope
    Added isotope to thumbnails

--- includes/footer.php
+++ includes/footer.php
@@ -10,9 +10,9 @@
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
     <script src="bootstrap/js/bootstrap.min.js"></script>
-    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
+
+    <script src="https://npmcdn.com/isotope-layout@3.0/dist/isotope.pkgd.min.js"></script>
+    <script src="js/js.js"></script>
   </body>
 </html>
--- index.php
+++ index.php
@@ -6,18 +6,31 @@
     <div class="page-header">
         <h1>Portfolio</h1>
     </div>
-    <div class="row">
-        <div class="col-sm-6 col-md-4">
-            <div class="thumbnail">
-                <img src="..." alt="...">
-                <div class="caption">
-                    <h3>Thumbnail label</h3>
-                    <p>Thumbnail caption</p>
-                    <p>
-                        <a href="#" class="btn btn-primary" role="button">About</a> 
-                        <a href="#" class="btn btn-default" role="button">View Project</a>
-                    </p>
-                </div>
+    <div class="button-group filter-button-group">
+        <button data-filter="*">show all</button>
+        <button data-filter=".web">web</button>
+    </div>
+    <div class="grid">
+        <div class="grid-item thumbnail web">
+            <img src="..." alt="...">
+            <div class="caption">
+                <h3>Thumbnail label</h3>
+                <p>Thumbnail caption</p>
+                <p>
+                    <a href="#" class="btn btn-primary" role="button">About</a> 
+                    <a href="#" class="btn btn-default" role="button">View Project</a>
+                </p>
+            </div>
+        </div>
+        <div class="grid-item thumbnail">
+            <img src="..." alt="...">
+            <div class="caption">
+                <h3>Thumbnail label</h3>
+                <p>Thumbnail caption</p>
+                <p>
+                    <a href="#" class="btn btn-primary" role="button">About</a> 
+                    <a href="#" class="btn btn-default" role="button">View Project</a>
+                </p>
             </div>
         </div>
     </div>
